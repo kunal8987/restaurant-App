7c7ba842e8592747a9224f4bc8dea3ee4c6913d1
Kunal Deotale <kdeotale4@gmail.com>
2024-01-04 20:09:59 +0530

    add dependancies and env veriable

--- server/index.js
+++ server/index.js
@@ -1,14 +1,24 @@
 const express = require("express");
+const cors = require("cors");
+const dotenv = require("dotenv");
 
+// INITIALIZATION OF EXPRESS APP
 const app = express();
 
+//DOTENV CONFIG
+dotenv.config();
+
+//MIDDLEWARE
 app.use(express.json());
+
+//CORS
+app.use(cors());
 
 app.get("/", (req, res) => {
   res.send("<h1> hello world!</h1>");
 });
 
-let PORT = 4500;
+let PORT = process.env.PORT || 4500;
 app.listen(PORT, (req, res) => {
-  console.log("server listening on port");
+  console.log(`server listening on ${PORT}`);
 });
