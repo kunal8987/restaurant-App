2868127427857045dc7f6707d2310ec9d5912f57
Kunal Deotale <kdeotale4@gmail.com>
2024-01-16 23:24:41 +0530

    get all restaurant , by id , delete functionality

--- server/src/router/restaurant.router.js
+++ server/src/router/restaurant.router.js
@@ -1,10 +1,28 @@
 const express = require("express");
 const { authentication } = require("../middleware/auth.middleware");
-const { createRestaurant } = require("../controller/restaurant.controller");
+const {
+  createRestaurant,
+  getAllRestaurantsData,
+  getRestaurantData,
+  deleteRestaurant,
+} = require("../controller/restaurant.controller");
 
 const restaurantRouter = express.Router();
 
 //CREATE RESTAURANT ROUTE
 restaurantRouter.post("/create-restaurant", authentication, createRestaurant);
+
+//GET ALL RESTAURANT ROUTE
+restaurantRouter.get("/get-all-restaurants", getAllRestaurantsData);
+
+//GET SINGLE RESTAURANT ROUTE
+restaurantRouter.get("/get-restaurants/:id", getRestaurantData);
+
+//DELETE SINGLE RESTAURANT ROUTE
+restaurantRouter.delete(
+  "/delete-restaurants/:id",
+  authentication,
+  deleteRestaurant
+);
 
 module.exports = { restaurantRouter };
